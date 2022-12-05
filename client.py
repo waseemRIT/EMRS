d23d2a4422b75985b2d22f440a1106897660f875
waseemRIT <89963412+waseemRIT@users.noreply.github.com>
2022-12-05 21:24:07 +0400

    CORRECT COMMENTS ADDED

--- client.py
+++ client.py
@@ -6,14 +6,14 @@
 
 import socket
 try:
-<<<<<<< HEAD
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # Specifying type of connection like TCP
     host = socket.gethostname() # gets the  address of client machine used to connect later on
     port = 9999 # 9999 is the communication port between client and server
     s.connect((host, port)) # connects the client with the server
 except socket.error as e:   # to handle any unexpected error during the process
     print(str(e))   # prints the error msg
-=======
+
     # Create a socket object
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Get local machine name
@@ -25,7 +25,7 @@ except socket.error as e:   # to handle any unexpected error during the process
 except socket.error as e:
     # Print error message
     print(str(e))
->>>>>>> c06465ca7132b8afe61540b1a68a1a0669953fa9
+
     
 # Send username and password to server
 username = input("Enter username: ")
@@ -36,7 +36,6 @@ password = input("Enter password: ")
 s.send(password.encode())
 # Continuously receive data from server
 while True:
-<<<<<<< HEAD
     data = s.recv(1024)     # used to received and store  the data
     print(data.decode())    # printing the received data
     # Check if data contains the word "Choice:"
@@ -46,8 +45,8 @@ while True:
     elif "Exiting" in data.decode():    # If Existing comes from the servers msg then client has requested to exit
         break
     elif "invalid Username or Password" in data.decode():   # To Exit after notifying user that username/password is
-        # Invalid
-=======
+        break # Invalid
+
     # Receive data from server
     data = s.recv(1024)
     # Print data
@@ -61,7 +60,6 @@ while True:
         break
     # Check if data contains the word "invalid username" then break
     elif "invalid username" in data.decode():
->>>>>>> c06465ca7132b8afe61540b1a68a1a0669953fa9
         break
 
 s.close()   # to close the socket between client and server
